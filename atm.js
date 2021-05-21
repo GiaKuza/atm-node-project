a57899b143636c80a72dfa4c9bc96454ccd08f8a
Giancarlo Coca <giancarlococa@Giancarlos-MacBook-Pro.local>
2021-05-20 17:09:30 -0700

    withdraw function implemented

--- atm.js
+++ atm.js
@@ -3,7 +3,7 @@
 
 const account = require('./account');
 var prompt = require('prompt-sync')();
-console.log(account);
+//console.log(account);
 
 function sleep(milliseconds) {
     const date = Date.now();
@@ -31,55 +31,89 @@ function validatePin(){
         return userInfo;
     }
     else{
-        console.log("Invalid Pin! Please verify the PIN number or Account number you entered is correct\n");
+        //console.log("Invalid Pin! Please verify the PIN number or Account number you entered is correct and try again\n");
         return false;
     }
 }
 
 
-function getBalance(){
+function getBalance(userInfo){
+    
+    //var currentBal = userInfo.accountBal;
+    /*
     //console.log(account.userData[0].accountBal)
    var userInfo = validatePin();
    if (userInfo != false){
        return userInfo.accountBal;
    }
-   return false;
-  
+   return false;*/
+  // console.log("Your current balance is " + currentBal);
+  return userInfo.accountBal;
 }
 
-function withdraw(){
-    var userInfo = validatePin();
-    var currentBal = userInfo.accountBal;
-    var amountToWithdraw = prompt("Current Balance: " + currentBal + "\nPlease enter the amount you would like to withdraw or press x to cancel this transaction: ");
-
+function withdraw(userInfo){
+    //var userInfo = validatePin();
+    var currentBal = getBalance(userInfo);
+    var amountToWithdraw = prompt("Current Balance: " + currentBal + "\nPlease enter the amount you would like to withdraw or press x to return to the main menu: ");
+/*
     if (amountToWithdraw == 'x'){
         console.log("Transaction Cancelled, returning to the main menu...")
         return;
     }
+*/
 
-    //var userInfo = validatePin(); //returns info of verified user
+     while(amountToWithdraw != 'x'){
+         if(parseInt(amountToWithdraw) > currentBal){
+            console.log("Error! The amount you wish to withdraw exceeds your current balance, please enter an amount lower than " + currentBal + "\n");
+        }
+         else{
+
+            console.log("Processing Request... ")
+            userInfo.accountBal -= amountToWithdraw;
+            
+            //testing is pass by ref or value
+            //var newBal = getBalance();
+            currentBal = getBalance(userInfo);
+            //console.log("new balance is: " + currentBal);
     
+            sleep(2000);
+            console.log("Success!.. your cash is now being dispensed... ");
+            sleep(2000);
+         }
+         amountToWithdraw = prompt("Current Balance: " + currentBal + "\nPlease enter the amount you would like to withdraw or press x to cancel this transaction: ");
+         //console.log(amountToWithdraw);
+     }   
+     console.log("OK! Returning to the main menu...");
+     return;
+    
+    }
+    //var userInfo = validatePin(); //returns info of verified user
+    /*
     while( parseInt(amountToWithdraw) > currentBal){
         console.log("Sorry! The amount you wish to withdraw exceeds your current balance, please enter an amount lower than " + currentBal + ":\n")
         amountToWithdraw = prompt("Current Balance: " + currentBal + "\nPlease enter the amount you would like to withdraw or press x to cancel this transaction: ");
         if (amountToWithdraw == 'x'){
             console.log("Transaction Cancelled, returning to the main menu...")
-            return;
+            amountToWithdraw = 0;
+            break;
         }
+        
     }
 
         console.log("Processing Request... ")
         userInfo.accountBal -= amountToWithdraw;
         //testing is pass by ref or value
-        var newBal = getBalance();
+        //var newBal = getBalance();
+        var newBal = getBalance(userInfo);
         console.log("new balance is: " + newBal);
 
         sleep(2000);
         console.log("Success!.. your cash is now being dispensed, dont forget to retrieve your card! ");
         sleep(2000);
-        console.log("Returning to the main menu")
-;    
-}
+       // console.log("Returning to the main menu")*/
+   //
+//}
+
 
 function deposit(){
 
@@ -111,6 +145,7 @@ module.exports = {
     getBalance : getBalance,
     withdraw : withdraw,
     deposit : deposit,
-    validatePin : validatePin 
+    validatePin : validatePin, 
+    sleep : sleep
 
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
-console.log("atm project!!")
+var prompt = require('prompt-sync')();
+const { sleep } = require('./atm');
 //const atm = require('./atm');
 const atmFunctions = require('./atm');
+
 /////var isValidated = atmFunctions.validatePin();
 //console.log(isValidated)
 
@@ -9,9 +11,63 @@ const atmFunctions = require('./atm');
 //console.log(atmFunctions.getBalance());
 
 //atmFunctions.withdraw();//
-atmFunctions.deposit();
+//atmFunctions.deposit();
+
+
 
 
 
 
 //write a menu 
+
+
+
+function displayUserMenu(){
+    
+    
+    let userInfoValidated = atmFunctions.validatePin();
+    console.log("Please wait... Verifying account information");
+    sleep(2000);
+
+    if(userInfoValidated){
+    console.log("\nAccess Granted!\n")
+    console.log("\nWelcome " + userInfoValidated.firstName + " " + userInfoValidated.lastName + ",")
+    sleep(2000);
+    
+    
+    console.log("\nPlease make a selection from the following menu:\n");    
+    var userSelection = prompt('\n(1) View Account Balance\n(2) Withdrawal\n(3) Deposits\n(4) Exit\n');
+    while(parseInt(userSelection) != 4){
+    switch (parseInt(userSelection)){
+        case 1:
+            atmFunctions.getBalance(userInfoValidated);
+            break;
+        case 2:
+            atmFunctions.withdraw(userInfoValidated);
+            break;
+        case 3:
+            atmFunctions.deposit();
+            break;
+        case 4:
+            return;
+        }
+        userSelection = prompt('\n(1) View Account Balance\n(2) Withdrawal\n(3) Deposits\n(4) Exit\n');
+       
+    }//end of while loop
+    }
+    else {
+        console.log("Invalid Account Number or PIN number, please check and try again;")
+        
+        
+    }
+
+}
+
+
+    console.log("\n\nWelcome to Tungsten Credit Union\n");
+    //let userInfo = null;
+    //userInfo = atmFunctions.validatePin();
+
+    displayUserMenu();
+    
+
